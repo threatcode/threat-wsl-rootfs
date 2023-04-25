0975cf5440ceb6240a7b80a9a3285f2d6feb3362
Ben Wilson <g0tmi1k@kali.org>
2023-04-25 13:32:25 +0100

    Complete overall of script

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,102 +1,204 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# ./0.sh
+# http_proxy= ./0.sh
+#
+# REF:
+# - https://gitlab.com/kalilinux/build-scripts/kali-docker/-/blob/master/build-rootfs.sh
+# - https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-project/-/blob/master/nethunter-fs/stages/stage1
+#
 
-set -e
+set -eu
 
-BUILDIR=$(pwd)
-TMPDIR_x64=$(mktemp -d)
-TMPDIR_ARM64=$(mktemp -d)
-MIRROR_REPO="http.kali.org"
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+KNOWN_CACHING_PROXIES="\
+3142 apt-cacher-ng
+8000 squid-deb-proxy
+9999 approx"
+DETECTED_CACHING_PROXY=
 
-# create_rootfs <x64|ARM64>
-create_rootfs() {
-  ARCH=${1:-x64}
+SUPPORTED_ARCHITECTURES="amd64 arm64"
+SUPPORTED_BRANCHES="kali-rolling kali-dev kali-last-snapshot"
+SUPPORTED_DESKTOPS="e17 gnome i3 kde lxde mate xfce none"
+SUPPORTED_TOOLSETS="default everything headless large none"
 
-  echo -e "\n  [i] Bootstrapping a base Kali install ($ARCH)"
-  if [ "$ARCH" == "ARM64" ]; then
-    cd "$TMPDIR_ARM64/"
-    ## Because we're using a foreign architecture (arm64 on amd64) we have to do the debootstrap in 2 stages
-    LANG=C debootstrap --foreign --arch arm64 kali-rolling ./kali-root "http://$MIRROR_REPO/kali"
-    cp -v /usr/bin/qemu-aarch64-static kali-root/usr/bin
-    LANG=C chroot kali-root /debootstrap/debootstrap --second-stage
-  elif [ "$ARCH" == "x64" ]; then
-    cd "$TMPDIR_x64/"
+DEFAULT_ARCH=amd64
+DEFAULT_BRANCH=kali-rolling
+DEFAULT_DESKTOP=none
+DEFAULT_MIRROR=http://http.kali.org/kali
+DEFAULT_TOOLSET=none
 
-    LANG=C debootstrap kali-rolling ./kali-root "http://$MIRROR_REPO/kali"
+ARCH=
+BRANCH=
+DESKTOP=
+KEEP=false
+MIRROR=
+PACKAGES=
+TOOLSET=
+VERSION=
+VARIANT=WSL
+OUTDIR="$( pwd )/output"
+PROMPT=#
+
+default_toolset() { [ ${DESKTOP:-$DEFAULT_DESKTOP} = none ] && echo none || echo ${DEFAULT_TOOLSET}; }
+default_version() { echo ${BRANCH:-$DEFAULT_BRANCH} | sed "s/^kali-//"; }
+
+## Output bold only if both stdout/stderr are opened on a terminal
+if [ -t 1 ] && [ -t 2 ]; then
+  b() { tput bold; echo -n "$@"; tput sgr0; }
+else
+  b() { echo -n "$@"; }
+fi
+vrun() { echo -n "[$( date -u +'%H:%M:%S' )] ${VARIANT}:~${PROMPT} "; b "$@"; echo; $@; }
+warn() { echo "WARNING: " "$@" 1>&2; }
+fail() { echo "ERROR: "   "$@" 1>&2; exit 1; }
+
+kali_message() {
+  local line=
+  echo   "┏━━($( b $@ ))"
+  while IFS= read -r line; do
+    echo "┃ ${line}";
+  done
+  echo   "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+}
+
+ask_confirmation() {
+  local question=${1:-"Do you want to continue?"}
+  local default=yes
+  local default_verbing=
+  local choices=
+  local grand_timeout=60
+  local timeout=20
+  local time_left=
+  local answer=
+  local ret=
+
+  ## If stdin is closed, no need to ask, assume yes
+  [ -t 0 ] || return 0
+
+  ## Set variables that depend on default
+  if [ ${default} = yes ]; then
+    default_verbing=proceeding
+    choices="[Y/n]"
   else
-    echo "[-] Unsupported architecture" >&2
-    exit 1
+    default_verbing=aborting
+    choices="[y/N]"
   fi
 
-  echo -e "\n  [i] Setting chroot's DNS"
-  cat << EOF > kali-root/etc/resolv.conf
-nameserver 8.8.8.8
-EOF
+  ## Discard chars pending on stdin
+  while read -r -t 0; do read -r; done
 
-  echo -e "\n  [i] Setting environment variables"
-  export MALLOC_CHECK_=0 # workaround for LP: #520465 (https://bugs.launchpad.net/ubuntu/+bug/520465)
+  ## Ask the question, allow for X timeouts before proceeding anyway
+  grand_timeout=$(( grand_timeout - timeout ))
+  for time_left in $( seq ${grand_timeout} -${timeout} 0 ); do
+    ret=0
+    read -r -t ${timeout} -p "${question} ${choices} " answer \
+      || ret=$?
+    if [ ${ret} -gt 128 ]; then
+      if [ ${time_left} -gt 0 ]; then
+        echo "...${time_left} seconds left before ${default_verbing}"
+      else
+        echo "...No answer, assuming ${default}, ${default_verbing}"
+      fi
+      continue
+    elif [ ${ret} -gt 0 ]; then
+      exit ${ret}
+    else
+      break
+    fi
+  done
+
+  ## Process the answer
+  [ "${answer}" ] && answer=${answer} || answer=${default}
+  case "${answer}" in
+    y|yes) return 0;;
+    *)     return 1;;
+  esac
+  echo ""
+}
+
+## check_os
+check_os() {
+  [ -e "/usr/share/debootstrap/scripts/${BRANCH}" ] \
+    || fail "debootstrap has no script for: ${BRANCH}. Need to use a newer debootstrap"
+
+  [ -e "/usr/share/keyrings/kali-archive-keyring.gpg" ] \
+    || fail "Missing /usr/share/keyrings/kali-archive-keyring.gpg (See README.md -> Non-Kali Debian-Based Environment)"
+}
+
+## rootfs_chroot <cmd>
+rootfs_chroot() {
+  echo "[$( date -u +'%H:%M:%S' )] (chroot) ${VARIANT}:~${PROMPT} $( b "$@" )"
+  PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+    chroot "${rootfsDir}" "$@"
+}
+
+## debootstrap_log <ret>
+debootstrap_log() {
+  if [ "${1}" != 0 ]; then
+    warn "debootstrap: exit $1"
+    [ -e "${rootfsDir}"/debootstrap/debootstrap.log ] && \
+      tail -v "${rootfsDir}"/debootstrap/debootstrap.log
+    exit "${1}"
+  fi
+}
+
+## create_rootfs
+create_rootfs() {
   export LC_ALL=C
-  export DEBIAN_FRONTEND=noninteractive
+  ## Workaround for https://bugs.launchpad.net/ubuntu/+bug/520465
+  export MALLOC_CHECK_=0
 
-  echo -e "\n  [i] Mounting for chroot"
-  mount -v -t proc proc kali-root/proc
-  mount -v -o bind /dev/ kali-root/dev/
-  mount -v -o bind /dev/pts kali-root/dev/pts
+  vrun mkdir -pv "${OUTDIR}/"
+  rootfsDir="$( mktemp -d )"
 
-  echo -e "\n  [i] Creating second-stage"
-  cat << EOF > kali-root/second-stage
-#!/bin/bash
-apt-get update
-apt-get --yes install locales-all mlocate sudo net-tools wget host dnsutils whois curl kali-defaults
-#apt-get --yes --force-yes install kali-desktop-xfce xorg xrdp
-rm -rf /root/.bash_history
-apt-get clean
-apt-get autoremove
+  cmd_args=""
+  ## If we are using a foreign architecture (e.g. arm64 on amd64) we are opt'ing to do debootstrap in 2 stages
+  ## Newer versions of debootstrap may be auto handle this in the future
+  if (
+       ([ "$( uname -m )" != "aarch64" ] && [ "${ARCH}" == "arm64" ]) ||
+       ([ "$( uname -m )" != "x86_64"  ] && [ "${ARCH}" == "amd64" ])
+     ); then
+    echo "[i] Foreign architecture: $( uname -m ) machine for ${ARCH} rootfs"
+    cmd_args="--foreign"
+  fi
+  ret=0
+  ## Install all packages of priority required and important, including apt. Skipping: --variant=minbase
+  vrun /usr/sbin/debootstrap \
+      ${cmd_args} \
+      --arch "${ARCH}" \
+      --components=main,contrib,non-free,non-free-firmware \
+      --include=kali-archive-keyring \
+      "${BRANCH}" \
+      "${rootfsDir}"/ \
+      "${MIRROR}" \
+    || debootstrap_log "$?"
 
-rm -f /0
-rm -f /hs_err*
-rm -f /cleanup
-rm -f /usr/bin/qemu*
+  if (
+       ([ "$( uname -m )" != "aarch64" ] && [ "${ARCH}" == "arm64" ]) ||
+       ([ "$( uname -m )" != "x86_64"  ] && [ "${ARCH}" == "amd64" ])
+     ); then
+    if [ "$( uname -m )" != "aarch64" ] && [ "${ARCH}" == "arm64" ]; then
+      vrun cp -v /usr/bin/qemu-aarch64-static "${rootfsDir}"/usr/bin
+    elif [ "$( uname -m )" != "x86_64" ] && [ "${ARCH}" == "amd64" ]; then
+      vrun cp -v /usr/bin/qemu-x86_64-static "${rootfsDir}"/usr/bin
+    else
+      fail "Unsure of cross-build architecture: $( uname -m ) / ${ARCH}"
+    fi
+    ret=0
+    rootfs_chroot /debootstrap/debootstrap \
+        --second-stage \
+      || debootstrap_log "$?"
+  fi
 
-updatedb
-EOF
-
-  echo -e "\n  [i] Executing second-stage (in chroot)"
-  chmod -v +x kali-root/second-stage
-  LANG=C chroot kali-root /second-stage
-
-  #echo -e "\n  [i] Setting up xrdp"
-  #sed -i 's/port=3389/port=3390/g' kali-root/etc/xrdp/xrdp.ini
-
-  echo -e "\n  [i] Unmounting chroot"
-  umount -v kali-root/dev/pts
-  sleep 3
-  umount -v kali-root/dev
-  sleep 10
-  umount -v kali-root/proc
-  sleep 5
-
-  echo -e "\n  [i] Cleaning up"
-  # cd kali-root
-  rm -vrf kali-root/second-stage
-  rm -vrf kali-root/etc/resolv.conf
-  rm -vrf kali-root/usr/bin/qemu-aarch64-static
-
-  echo -e "\n  [i] Setting repo source"
-  cat <<EOF > kali-root/etc/apt/sources.list
-deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
-EOF
-
-## Gamb1t 8/15/22: Do not remove the `'` around EOF. This will cause errors in a built image.
-
-  echo -e "\n  [i] Setting shell profile"
-  cat << 'EOF' > kali-root/etc/profile
+  echo "[i] Setting shell profile"
+  cat << EOF > "${rootfsDir}"/etc/profile
 # /etc/profile: system-wide .profile file for the Bourne shell (sh(1))
 # and Bourne compatible shells (bash(1), ksh(1), ash(1), ...).
 
-IS_WSL=`grep -i microsoft /proc/version` # WSL already sets PATH, shouldn't be overridden
-if test "$IS_WSL" = ""; then
-  if [ "`id -u`" -eq 0 ]; then
+# WSL already sets PATH, shouldn't be overridden
+IS_WSL=\$( grep -i microsoft /proc/version )
+if test "\${IS_WSL}" = ""; then
+  if [ "\$( id -u )" -eq 0 ]; then
     PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
   else
     PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
@@ -104,15 +206,15 @@ if test "$IS_WSL" = ""; then
 fi
 export PATH
 
-if [ "${PS1-}" ]; then
-  if [ "${BASH-}" ] && [ "$BASH" != "/bin/sh" ]; then
+if [ "$\{PS1-}" ]; then
+  if [ "\${BASH-}" ] && [ "\${BASH}" != "/bin/sh" ]; then
     # The file bash.bashrc already sets the default PS1.
-    # PS1='\h:\w\$ '
+    # PS1='\h:\w$ '
     if [ -f /etc/bash.bashrc ]; then
       . /etc/bash.bashrc
     fi
   else
-    if [ "`id -u`" -eq 0 ]; then
+    if [ "\$( id -u )" -eq 0 ]; then
       PS1='# '
     else
       PS1='$ '
@@ -122,25 +224,237 @@ fi
 
 if [ -d /etc/profile.d ]; then
   for i in /etc/profile.d/*.sh; do
-    if [ -r $i ]; then
-      . $i
+    if [ -r \$i ]; then
+      . \$i
     fi
   done
   unset i
 fi
 EOF
 
-  echo -e "\n  [i] Compressing chroot: $BUILDIR/$ARCH/install.tar.gz"
-  mkdir -vp "$BUILDIR/$ARCH/"
-  cd kali-root/
-  tar --ignore-failed-read -czf "$BUILDIR/$ARCH/install.tar.gz" ./*
+  rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes install kali-defaults # Skipping: --no-install-recommends
+  rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes install locales-all mlocate sudo net-tools wget host dnsutils whois curl # Should be in a kali-meta
+
+  [ "${TOOLSET}" != "none" ] && \
+    rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes install kali-linux-${TOOLSET}
+  [ "${DESKTOP}" != "none" ] && \
+    rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes install kali-desktop-${DESKTOP} xorg xrdp
+  [ "${PACKAGES}" ] && \
+    rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes install ${PACKAGES}
+
+  if [ "${DESKTOP}" != "none" ]; then
+    echo "[i] Switching xrdp to use 3390/TCP"
+    vrun sed -i 's/port=3389/port=3390/g' "${rootfsDir}"/etc/xrdp/xrdp.ini
+  fi
+
+  ## Using pipes with vrun doesn't work too well
+  #echo "deb ${DEFAULT_MIRROR} ${BRANCH} main contrib non-free non-free-firmware" > "${rootfsDir}"/etc/apt/sources.list
+  cat << EOF > "${rootfsDir}"/etc/apt/sources.list
+# See: https://www.kali.org/docs/general-use/kali-linux-sources-list-repositories/
+deb ${DEFAULT_MIRROR} ${BRANCH} main contrib non-free non-free-firmware
+
+# Additional line for source packages
+#deb-src ${DEFAULT_MIRROR} ${BRANCH} main contrib non-free non-free-firmware
+EOF
+  echo "kali" > "${rootfsDir}"/etc/hostname
+  #echo "127.0.0.1 localhost" > "${rootfsDir}"/etc/hosts
+  cat << EOF > "${rootfsDir}"/etc/hosts
+127.0.0.1 localhost
+::1   localhost ip6-localhost ip6-loopback
+ff02::1   ip6-allnodes
+ff02::2   ip6-allrouters
+EOF
+  vrun truncate -s 0 "${rootfsDir}"/etc/resolv.conf
+
+  ## Clean - APT packages
+  rootfs_chroot env DEBIAN_FRONTEND=noninteractive apt-get --quiet --yes clean
+  vrun rm -vrf "${rootfsDir}"/var/lib/apt/lists/*
+  vrun mkdir -vp "${rootfsDir}"/var/lib/apt/lists/partial
+  ## Clean - Logs
+  ! "${KEEP}" && \
+    vrun find "${rootfsDir}"/var/log -depth -type f -exec truncate -s 0 {} +
+  ## Clean - qemu
+  (
+    ([ "$( uname -m )" != "aarch64" ] && [ "${ARCH}" == "arm64" ]) ||
+    ([ "$( uname -m )" != "x86_64"  ] && [ "${ARCH}" == "amd64" ])
+  ) && \
+     vrun rm -vf "${rootfsDir}"/usr/bin/qemu*
+  ## Clean - Misc
+  vrun rm -vf "${rootfsDir}"/var/cache/ldconfig/aux-cache
+
+  vrun pushd "${rootfsDir}/"
+  ## Skipping tar -v: too noisy
+  ## Skipping tar -C "${rootfsDir}"/ / --exclude=./: Due './' being added (tar -tvfj output/*tar.gz | sort -k 9 | head)
+  vrun tar --ignore-failed-read -czf "${OUTDIR}/${OUTPUT}.tar.gz" ./"*"
+  vrun popd
+
+  if ! "${KEEP}"; then
+    ## Skipping rm -v - too noisy
+    vrun rm -rf "${rootfsDir}"/
+  fi
 }
 
-echo -e "\n  [i] Kali WSL (Chroot)"
+USAGE="Usage: $( basename $0 ) <options>
 
-create_rootfs x64
-create_rootfs ARM64
+Build a Kali Linux ${VARIANT} rootfs
 
-echo -e "\n  [i] Cleaning up temporary build folders"
-rm -rf "$TMPDIR_x64/"
-rm -rf "$TMPDIR_ARM64/"
+Build options:
+  -a ARCH     Build an rootfs for this architecture, default: $( b ${DEFAULT_ARCH} )
+              Supported values: ${SUPPORTED_ARCHITECTURES}
+  -b BRANCH   Kali branch used to build the rootfs, default: $( b ${DEFAULT_BRANCH} )
+              Supported values: ${SUPPORTED_BRANCHES}
+  -k          Keep intermediary build artifacts
+  -m MIRROR   Mirror used to build the rootfs, default: $( b ${DEFAULT_MIRROR} )
+  -x VERSION  What to name the rootfs release as, default: $( b $( default_version ) )
+
+Customization options:
+  -D DESKTOP  Desktop environment installed in the rootfs, default: $( b ${DEFAULT_DESKTOP} )
+              Supported values: ${SUPPORTED_DESKTOPS}
+  -P PACKAGES Install extra packages (comma/space separated list)
+  -T TOOLSET  The selection of tools to include in the rootfs, default: $( b $( default_toolset ) )
+              Supported values: ${SUPPORTED_TOOLSETS}
+
+Supported environment variables:
+  http_proxy  HTTP proxy URL, refer to the README.md for more details
+
+Refer to the README.md for examples"
+
+while getopts ":a:b:D:f:hkL:m:P:r:s:T:U:v:x:zZ:" opt; do
+  case ${opt} in
+    (a) ARCH=${OPTARG};;
+    (b) BRANCH=${OPTARG};;
+    (D) DESKTOP=${OPTARG};;
+    (h) echo "${USAGE}"; exit 0;;
+    (k) KEEP=true;;
+    (m) MIRROR=${OPTARG};;
+    (P) PACKAGES="${PACKAGES} ${OPTARG}";;
+    (T) TOOLSET=${OPTARG};;
+    (x) VERSION=${OPTARG};;
+    (*) echo "${USAGE}" 1>&2; exit 1;;
+  esac
+done
+shift $((OPTIND - 1))
+
+## If there isn't any variables setup, use default
+[ "${ARCH}"    ] || ARCH=${DEFAULT_ARCH}
+[ "${BRANCH}"  ] || BRANCH=${DEFAULT_BRANCH}
+[ "${DESKTOP}" ] || DESKTOP=${DEFAULT_DESKTOP}
+[ "${MIRROR}"  ] || MIRROR=${DEFAULT_MIRROR}
+[ "${TOOLSET}" ] || TOOLSET=$( default_toolset )
+[ "${VERSION}" ] || VERSION=$( default_version )
+
+TOOLSET="$( echo ${TOOLSET} | sed 's/kali-linux-//' )"
+
+case $( echo ${ARCH}| tr '[:upper:]' '[:lower:]' ) in
+  x64|x86_64|x86-64|amd64)
+    ARCH=amd64
+    ;;
+  arm64|aarch64)
+    ARCH=arm64
+    ;;
+esac
+
+case ${BRANCH} in
+  kali-last-release|kali-last-snapshot)
+    BRANCH=kali-last-snapshot
+    ;;
+esac
+
+## Order packages alphabetically, separate each package with ", "
+PACKAGES=$( echo ${PACKAGES} | sed "s/[, ]\+/\n/g" | LC_ALL=C sort -u \
+  | awk 'ORS=", "' | sed "s/[, ]*$//" )
+
+## Filename structure for final file
+OUTPUT=$( echo "kali-linux-${VERSION}-${VARIANT}-rootfs-${ARCH}" | tr '[:upper:]' '[:lower:]' )
+
+## Validate some options
+echo "${SUPPORTED_BRANCHES}" | grep -qw "${BRANCH}" \
+  || fail "Unsupported branch: ${BRANCH}"
+echo "${SUPPORTED_DESKTOPS}" | grep -qw "${DESKTOP}" \
+  || fail "Unsupported desktop: ${DESKTOP}"
+echo "${SUPPORTED_TOOLSETS}" | grep -qw "${TOOLSET}" \
+  || fail "Unsupported toolset: ${TOOLSET}"
+echo "${SUPPORTED_ARCHITECTURES}" | grep -qw "${ARCH}" \
+  || fail "Unsupported architecture: ${ARCH}"
+
+## Check environment variables for http_proxy
+## [ -v ... ] isn't supported on all every bash version
+if ! [ $( env | grep http_proxy ) ]; then
+  ## Attempt to detect well-known http caching proxies on localhost,
+  ## cf. bash(1) section "REDIRECTION". This is not bullet-proof.
+  while read port proxy; do
+    (</dev/tcp/localhost/${port}) 2>/dev/null || continue
+    DETECTED_CACHING_PROXY="${port} ${proxy}"
+## Docker: host.docker.internal TODO
+    export http_proxy="http://127.0.0.1:${port}"
+    break
+  done <<< "${KNOWN_CACHING_PROXIES}"
+fi
+
+check_os
+
+if [ $( id -u ) -ne 0 ]; then
+  PROMPT=$
+  warn "This script requires certain privileges"
+  warn "Please consider running it using the root user"
+  echo ""
+fi
+
+## Print a summary
+{
+echo "# Proxy configuration:"
+if [ "${DETECTED_CACHING_PROXY}" ]; then
+  read port proxy <<< ${DETECTED_CACHING_PROXY}
+  echo " * Detected caching proxy $( b ${proxy} ) on port $( b ${port} )"
+elif [ "${http_proxy:-}" ]; then
+  echo " * Using proxy via environment variable: $( b http_proxy=${http_proxy} )"
+else
+  echo " * $( b No HTTP proxy ) configured, all packages will be downloaded from the Internet"
+fi
+
+echo "# ${VARIANT} rootfs output:"
+echo " * Build a Kali Linux ${VARIANT} rootfs for $( b ${ARCH} ) architecture"
+echo "# Build options:"
+[ "${MIRROR}"   ] && echo " * Build mirror: $( b ${MIRROR} )"
+[ "${BRANCH}"   ] && echo " * Branch: $( b ${BRANCH} )"
+[ "${VERSION}"  ] && echo " * Version: $( b ${VERSION} )"
+[ "${DESKTOP}"  ] && echo " * Desktop environment: $( b ${DESKTOP} )"
+[ "${TOOLSET}"  ] && echo " * Tool selection: $( b ${TOOLSET} )"
+[ "${PACKAGES}" ] && echo " * Additional packages: $( b ${PACKAGES} )"
+  "${KEEP}"       && echo " * Keep temporary files: $( b ${KEEP} )"
+} | kali_message "Kali Linux ${VARIANT} rootfs"
+
+## Ask for confirmation before starting the build
+ask_confirmation || { echo "Abort"; exit 1; }
+
+## Build
+create_rootfs
+
+## Finish
+cat << EOF
+..............
+            ..,;:ccc,.
+          ......''';lxO.
+.....''''..........,:ld;
+           .';;;:::;,,.x,
+      ..'''.            0Xxoc:,.  ...
+  ....                ,ONkc;,;cokOdc',.
+ .                   OMo           ':$( b dd )o.
+                    dMc               :OO;
+                    0M.                 .:o.
+                    ;Wd
+                     ;XO,
+                       ,d0Odlc;,..
+                           ..',;:cdOOd::,.
+                                    .:d;.':;.
+                                       'd,  .'
+                                         ;l   ..
+                                          .o
+                                            c
+                                            .'
+                                             .
+Successful build! The following build artifacts were produced:
+EOF
+## /recipes/ is due to container volume mount
+find "${OUTDIR}/" -maxdepth 1 -type f -name "${OUTPUT}*" | sed 's_^/recipes/__;
+                                                                s_^_* _'
